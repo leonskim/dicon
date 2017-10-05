0ab091eecbf096ba78e865506b9a9f15e7ad2470
Leon Kim <minusnine@gmail.com>
2017-10-05 17:35:26 +0900

    Add test buttons

--- App.js
+++ App.js
@@ -9,29 +9,38 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  NativeModules
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
+
+  buttonOnPress = (idx) => {
+    switch (idx) {
+      case 1:
+        break
+      case 2:
+        break
+      case 3:
+        break
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TouchableOpacity style={styles.button} onPress={() => this.buttonOnPress(1)}>
+          <Text>IconA</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => this.buttonOnPress(2)}>
+          <Text>IconB</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => this.buttonOnPress(3)}>
+          <Text>IconC</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -40,18 +49,14 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  button: {
+    backgroundColor: '#AA88BB',
+    padding: 10,
+    margin: 10
   },
 });
